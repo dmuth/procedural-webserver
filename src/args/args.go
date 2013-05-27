79b0f837aa11a651669829a1831d31f5acaebfe1
Douglas Muth <dmuth@dmuth.org>
2013-05-27 17:25:03 -0400

    Added sanity checking for cases where max < min.

--- src/args/args.go
+++ src/args/args.go
@@ -2,17 +2,18 @@
 package args
 
 import "flag"
+import "fmt"
 import "os"
 
 
 //
 // Configuration for what was passed in on the command line.
 //
-type config struct {
-	NumLinksMin int
-	NumLinksMax int
-	NumImagesMin int
-	NumImagesMax int
+type Config struct {
+	NumLinksMin uint
+	NumLinksMax uint
+	NumImagesMin uint
+	NumImagesMax uint
 	//MaxLevels int,
 	//NumKeyValuePairs
 }
@@ -22,20 +23,40 @@ type config struct {
 * Parse our command line arguments.
 * @return {config} Our configuration info
 */
-func Parse() (retval config) {
+func Parse() (retval Config) {
 
-	retval = config{0, 0, 0, 0}
-	flag.IntVar(&retval.NumLinksMin, "num-links-min", 1, 
+	retval = Config{0, 0, 0, 0}
+	flag.UintVar(&retval.NumLinksMin, "num-links-min", 1, 
 		"Minimum number of links per page")
-	flag.IntVar(&retval.NumLinksMax, "num-links-max", 1,
+	flag.UintVar(&retval.NumLinksMax, "num-links-max", 0,
 		"Maximum number of links per page")
-	flag.IntVar(&retval.NumImagesMin, "num-images-min", 1,
+	flag.UintVar(&retval.NumImagesMin, "num-images-min", 1,
 		"Minimum number of image links per page")
-	flag.IntVar(&retval.NumImagesMax, "num-images-max", 1,
+	flag.UintVar(&retval.NumImagesMax, "num-images-max", 0,
 		"Maximum number of image links per page")
 	h := flag.Bool("h", false, "To get this help")
 	help := flag.Bool("help", false, "To get this help")
 	flag.Parse()
+
+	if (retval.NumLinksMax == 0) {
+		retval.NumLinksMax = 1
+	}
+
+	if (retval.NumImagesMax == 0) {
+		retval.NumImagesMax = 1
+	}
+
+	if (retval.NumLinksMax < retval.NumLinksMin) {
+		panic(fmt.Sprintf(
+			"Max num links (%d) is less than min num links (%d)!",
+			retval.NumLinksMax, retval.NumLinksMin))
+	}
+
+	if (retval.NumImagesMax < retval.NumImagesMin) {
+		panic(fmt.Sprintf(
+			"Max num images (%d) is less than min num images (%d)!",
+			retval.NumImagesMax, retval.NumImagesMin))
+	}
 
 	if (*h || *help) {
 		flag.PrintDefaults()
