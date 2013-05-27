5648e9324b0f7a521a6d0ba928ef278110ecabbe
Douglas Muth <dmuth@dmuth.org>
2013-05-27 17:53:51 -0400

    Refactored code into getLinks() and getImages()

--- src/html/html.go
+++ src/html/html.go
@@ -2,7 +2,6 @@
 package html
 
 import "fmt"
-import "time"
 
 import "../args"
 import rand "../random_sorta"
@@ -18,43 +17,10 @@ import log "github.com/dmuth/google-go-log4go"
 func Html(config args.Config) (retval string) {
 
 	//
-	// Determine how many links go on this page
-	//
-	num_links := uint(1)
-	if (config.NumLinksMin != config.NumLinksMax) {
-		diff := config.NumLinksMax - config.NumLinksMin
-		random := rand.New( uint(time.Now().Nanosecond()), uint(diff) )
-		num_links = random.Intn()
-		num_links += config.NumLinksMin
-	}
-	log.Debug(fmt.Sprintf("Number of links on page: %d", num_links))
-
-	//
-	// How many images on the page?
-	//
-	num_images := uint(1)
-	if (config.NumImagesMin != config.NumImagesMax) {
-		diff := config.NumImagesMax - config.NumImagesMin
-		random := rand.New(uint(time.Now().Nanosecond()), uint(diff))
-		num_images = random.Intn()
-		num_images += uint(config.NumImagesMin)
-	}
-	log.Debug(fmt.Sprintf("Number of images on page: %d", num_images))
-
-	//
 	// Create the actual links and images
 	//
-	random := rand.New(10, uint(time.Now().Nanosecond()))
-
-	for i:=uint(0); i<num_links; i++ {
-		str := random.StringLowerN(10)
-		retval += fmt.Sprintf("<a href=\"/%s\" >%s</a>\n", str, str);
-	}
-
-	for i:=uint(0); i<num_images; i++ {
-		str := random.StringLowerN(10)
-		retval += fmt.Sprintf("<img src=\"/%s.png\" alt=\"%s\" />\n", str, str);
-	}
+	retval += getLinks(config)
+	retval += getImages(config)
 
 	retval = "<html>\n" + 
 		"<head><title></title></head>\n" +
@@ -66,6 +32,64 @@ func Html(config args.Config) (retval string) {
 	return(retval)
 
 } // End of Html()
+
+
+/**
+* Generate our links. 
+*/
+func getLinks(config args.Config) (retval string) {
+
+	//
+	// Determine how many links go on this page
+	//
+	num_links := uint(1)
+	if (config.NumLinksMin != config.NumLinksMax) {
+		diff := config.NumLinksMax - config.NumLinksMin
+		random := rand.New( config.Seed, uint(diff) )
+		num_links = random.Intn()
+		num_links += config.NumLinksMin
+	}
+	log.Debug(fmt.Sprintf("Number of links on page: %d", num_links))
+
+	random := rand.New( config.Seed, 100 )
+	for i:=uint(0); i<num_links; i++ {
+		str := random.StringLowerN(10)
+		retval += fmt.Sprintf("<a href=\"/%s\" >%s</a>\n", str, str);
+	}
+
+	return(retval);
+
+} // End of getLinks()
+
+
+/**
+* Generate our images.
+*/
+func getImages(config args.Config) (retval string) {
+
+	//
+	// How many images on the page?
+	//
+	num_images := uint(1)
+	if (config.NumImagesMin != config.NumImagesMax) {
+		diff := config.NumImagesMax - config.NumImagesMin
+		random := rand.New( config.Seed, uint(diff) )
+		num_images = random.Intn()
+		num_images += uint(config.NumImagesMin)
+	}
+	log.Debug(fmt.Sprintf("Number of images on page: %d", num_images))
+
+	random := rand.New( config.Seed, 100 )
+	for i:=uint(0); i<num_images; i++ {
+		str := random.StringLowerN(10)
+		retval += fmt.Sprintf(
+			"<img src=\"/%s.png\" alt=\"%s\" title=\"%s\" />\n", 
+			str, str, str);
+	}
+
+	return(retval)
+
+} // End of getImages()
 
 
 
