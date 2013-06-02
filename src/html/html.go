7a1f77a042923ead803d0a56f83fec2b0cfe779e
Douglas Muth <dmuth@dmuth.org>
2013-06-02 13:13:21 -0400

    Ported in changes for the new random functions.

--- src/html/html.go
+++ src/html/html.go
@@ -11,7 +11,9 @@ import log "github.com/dmuth/google-go-log4go"
 type Html_struct struct {
 	config args.Config
 	random_num_links rand.Random_struct
+	num_links_diff uint
 	random_num_images rand.Random_struct
+	num_images_diff uint
 	random_chars rand.Random_struct
 }
 
@@ -21,18 +23,20 @@ type Html_struct struct {
 */
 func New(config args.Config) (retval Html_struct) {
 
-	diff := config.NumLinksMax - config.NumLinksMin
+	num_links_diff := config.NumLinksMax - config.NumLinksMin
 	seed := config.Seed + 1
-	random_num_links := rand.New( seed, uint(diff) )
+	random_num_links := rand.New(seed)
 
-	diff = config.NumImagesMax - config.NumImagesMin
+	num_images_diff := config.NumImagesMax - config.NumImagesMin
 	seed = config.Seed + 2
-	random_num_images := rand.New( seed, uint(diff) )
+	random_num_images := rand.New(seed)
 
-	random_chars := rand.New( config.Seed, 100 )
+	random_chars := rand.New(config.Seed)
 
-	retval = Html_struct{config, 
-		random_num_links, random_num_images, random_chars}
+	retval = Html_struct{config,
+		random_num_links, num_links_diff,
+		random_num_images, num_images_diff,
+		random_chars	}
 
 	return(retval)
 
@@ -74,7 +78,7 @@ func (h *Html_struct) getLinks(config args.Config) (retval string) {
 	//
 	num_links := uint(1)
 	if (config.NumLinksMin != config.NumLinksMax) {
-		num_links = h.random_num_links.Intn()
+		num_links = h.random_num_links.Intn(h.num_links_diff)
 		num_links += config.NumLinksMin
 	}
 	log.Debug(fmt.Sprintf("Number of links on page: %d", num_links))
@@ -99,7 +103,7 @@ func (h *Html_struct) getImages(config args.Config) (retval string) {
 	//
 	num_images := uint(1)
 	if (config.NumImagesMin != config.NumImagesMax) {
-		num_images = h.random_num_images.Intn()
+		num_images = h.random_num_images.Intn(h.num_images_diff)
 		num_images += uint(config.NumImagesMin)
 	}
 	log.Debug(fmt.Sprintf("Number of images on page: %d", num_images))
